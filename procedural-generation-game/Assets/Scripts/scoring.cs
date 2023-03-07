3722dcf84559ff1465694dc763bc8bb4c59277ca
Dahriel-Gonzalez <99700458+Dahriel-Gonzalez@users.noreply.github.com>
2023-03-07 15:11:14 -0500

    scoring UI

--- procedural-generation-game/Assets/Scripts/scoring.cs
+++ procedural-generation-game/Assets/Scripts/scoring.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class scoring : MonoBehaviour
 {
     public int score;
     public float timer;
+    public TextMeshProUGUI UIscore;
+    public TextMeshProUGUI UItimer;
+    private int tier = 0;
+    private int nextTier = 100;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +21,14 @@ public class scoring : MonoBehaviour
     void Update()
     {
         timer -= (int) (1 * Time.deltaTime);
+        UIscore.SetText("Score:" + score.ToString());
+        UItimer.SetText("Timer:" + timer.ToString());
+        if (score > nextTier)
+        {
+            tier++;
+            nextTier += nextTier;
+            //grow arm
+        }
     }
 
     public void Scored(int newScore)
